692f50dc61445ec6202f01c9c5b1861ee64280c1
mrtbgau <mrt.bgau@gmail.com>
2025-09-17 14:52:02 +0200

    ✨ Add splash screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,27 @@
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+
+SplashScreen.setOptions({
+  duration: 1000,
+  fade: true,
+});
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [loaded] = useFonts({});
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hide();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
   return <Stack />;
 }
