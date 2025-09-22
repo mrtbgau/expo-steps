e3fa3d51ee2003437d3da591ab89c3a2ac7411b0
mrtbgau <mrt.bgau@gmail.com>
2025-09-22 11:41:21 +0200

    💄 Create tab bar layout

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,56 @@
+import { Feather, FontAwesome } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: "#1dd1a1",
+        tabBarInactiveTintColor: "#617989",
+        tabBarLabelStyle: {
+          fontSize: 12,
+          fontWeight: "500",
+          letterSpacing: 0.015,
+        },
+        tabBarStyle: {
+          backgroundColor: "#ffffff",
+          borderTopColor: "#f0f3f4",
+          borderTopWidth: 1,
+          paddingTop: 8,
+          paddingBottom: 12,
+          paddingHorizontal: 16,
+          height: 65,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="trips"
+        options={{
+          title: "Voyages",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome name="suitcase" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="map"
+        options={{
+          title: "Carte",
+          tabBarIcon: ({ color }) => (
+            <Feather name="map" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profil",
+          tabBarIcon: ({ color }) => (
+            <Feather name="user" size={24} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
