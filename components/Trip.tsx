b353b3789f77a953a0e050a173c5eea095ea1de5
mrtbgau <mrt.bgau@gmail.com>
2025-09-23 12:01:45 +0200

    💄 Create Trip component

---
+++ components/Trip.tsx
@@ -0,0 +1,65 @@
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+type Props = {
+  title: string;
+  duration: string;
+  dates: string;
+  image: string;
+};
+
+export default function Trip({ title, duration, dates, image }: Props) {
+  return (
+    <TouchableOpacity style={styles.tripCard} activeOpacity={0.7}>
+      <View style={styles.tripInfo}>
+        <Text style={styles.duration}>{duration}</Text>
+        <Text style={styles.title}>{title}</Text>
+        <Text style={styles.dates}>{dates}</Text>
+      </View>
+      <Image source={{ uri: image }} style={styles.tripImage} />
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  tripCard: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "stretch",
+    marginHorizontal: 16,
+    marginVertical: 8,
+    gap: 16,
+    backgroundColor: "#ffffff",
+    borderRadius: 12,
+    padding: 4,
+  },
+  tripInfo: {
+    flex: 2,
+    justifyContent: "center",
+    gap: 4,
+    paddingLeft: 4,
+  },
+  duration: {
+    fontSize: 14,
+    color: "#617989",
+    fontWeight: "400",
+    lineHeight: 16,
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: "700",
+    color: "#111518",
+    lineHeight: 20,
+  },
+  dates: {
+    fontSize: 14,
+    color: "#617989",
+    fontWeight: "400",
+    lineHeight: 16,
+  },
+  tripImage: {
+    flex: 1,
+    aspectRatio: 16 / 9,
+    borderRadius: 12,
+    backgroundColor: "#f0f0f0",
+  },
+});
