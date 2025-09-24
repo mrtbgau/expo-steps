ea04ff73066520946f1fd8d62e5ec701e778296f
mrtbgau <mrt.bgau@gmail.com>
2025-09-24 22:33:11 +0200

    ✨  Add account deletion functionality to Profile component

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -3,13 +3,57 @@ import Header from "@/components/Header";
 import Avatar from "@/components/profile/Avatar";
 import ProfileSection from "@/components/profile/ProfileSection";
 import { useAuth } from "@/contexts/AuthContext";
+import { useRouter } from "expo-router";
 import { useState } from "react";
 import { Alert, StyleSheet, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 export default function Profile() {
-  const { user } = useAuth();
+  const { user, deleteAccount, logout } = useAuth();
   const userMail = user?.email || "default@example.com";
   const [notificationsEnabled, setNotificationsEnabled] = useState(false);
+  const router = useRouter();
+
+  const handleDeleteAccount = () => {
+    Alert.alert(
+      "Supprimer le compte",
+      "Êtes-vous sûr de vouloir supprimer votre compte ? Cette action est irréversible.",
+      [
+        {
+          text: "Annuler",
+          style: "cancel",
+        },
+        {
+          text: "Supprimer",
+          style: "destructive",
+          onPress: async () => {
+            try {
+              await deleteAccount();
+              Alert.alert(
+                "Compte supprimé",
+                "Votre compte a été supprimé avec succès."
+              );
+              router.navigate("/");
+            } catch {
+              Alert.alert(
+                "Erreur",
+                "Une erreur est survenue lors de la suppression du compte. {error.message}"
+              );
+            }
+          },
+        },
+      ]
+    );
+  };
+
+  const handleLogout = async () => {
+    try {
+      await logout();
+      Alert.alert("Déconnecté", "Vous avez été déconnecté avec succès.");
+      router.navigate("/");
+    } catch {
+      Alert.alert("Erreur", "Une erreur est survenue lors de la déconnexion.");
+    }
+  };
 
   return (
     <SafeAreaView style={styles.safeArea}>
@@ -46,13 +90,13 @@ export default function Profile() {
       <View style={styles.buttonContainer}>
         <Button
           label="Me déconnecter"
-          onPress={() => Alert.alert("Déconnecté !")}
+          onPress={handleLogout}
           variant="btnSecondary"
           color="black"
         />
         <Button
           label="Supprimer mon compte"
-          onPress={() => Alert.alert("Compte supprimé !")}
+          onPress={handleDeleteAccount}
           variant="btnDanger"
           color="white"
         />
