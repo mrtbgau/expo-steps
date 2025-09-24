bd57577de7aec84159e2bcf56178782b07f990f4
mrtbgau <mrt.bgau@gmail.com>
2025-09-24 14:29:10 +0200

    💄 Refactor Profile component to improve user email handling and add notifications settings

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,15 +1,44 @@
 import Header from "@/components/Header";
 import Avatar from "@/components/profile/Avatar";
+import ProfileSection from "@/components/profile/ProfileSection";
 import { useAuth } from "@/contexts/AuthContext";
-import { StyleSheet } from "react-native";
+import { useState } from "react";
+import { Alert, StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Profile() {
   const { user } = useAuth();
+  const userMail = user?.email || "default@example.com";
+  const [notificationsEnabled, setNotificationsEnabled] = useState(false);
   return (
     <SafeAreaView style={styles.safeArea}>
       <Header title="Mon profil" />
-      <Avatar userMail={user?.email || "default@example.com"} />
+      <Avatar userMail={userMail} />
+      <ProfileSection
+        title="Compte"
+        rows={[
+          { label: "Email", value: userMail },
+          {
+            label: "Mot de passe",
+            action: {
+              text: "Modifier",
+              onPress: () => Alert.alert("Mot de passe modifié !"),
+            },
+          },
+        ]}
+      />
+      <ProfileSection
+        title="Paramètres"
+        rows={[
+          {
+            label: "Notifications",
+            toggle: {
+              value: notificationsEnabled,
+              onValueChange: setNotificationsEnabled,
+            },
+          },
+        ]}
+      />
     </SafeAreaView>
   );
 }
