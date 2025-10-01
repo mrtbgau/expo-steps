9a2a51a1623e21ac69cffb5a00a8b9535c6726ba
mrtbgau <mrt.bgau@gmail.com>
2025-10-01 20:31:17 +0200

    🏗️ Wrap Slot component with TripProvider to manage trip-related context in RootLayout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,15 @@
 import { Slot } from "expo-router";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { AuthProvider } from "../contexts/AuthContext";
+import { TripProvider } from "../contexts/TripContext";
 
 export default function RootLayout() {
   return (
     <GestureHandlerRootView>
       <AuthProvider>
-        <Slot />
+        <TripProvider>
+          <Slot />
+        </TripProvider>
       </AuthProvider>
     </GestureHandlerRootView>
   );
