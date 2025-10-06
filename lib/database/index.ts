c4db8edae0a4c345992a2f47f73b772fc53a78bc
mrtbgau <mrt.bgau@gmail.com>
2025-10-06 21:41:59 +0200

    🌱 Update database exports to include Stop type and stopService

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,4 +1,5 @@
 export { userService } from "./users";
 export { tripService } from "./trips";
+export { stopService } from "./stops";
 export { dbConnection } from "./connection";
-export type { User, Trip } from "./types";
+export type { User, Trip, Stop } from "./types";
