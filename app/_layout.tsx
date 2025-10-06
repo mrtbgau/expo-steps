9ec107a10bace4716feec30a92b76a7e079d2e4d
mrtbgau <mrt.bgau@gmail.com>
2025-10-06 21:42:42 +0200

    🏗️ Wrap Slot component with StopProvider for improved context management

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,13 +2,16 @@ import { Slot } from "expo-router";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { AuthProvider } from "../contexts/AuthContext";
 import { TripProvider } from "../contexts/TripContext";
+import { StopProvider } from "../contexts/StopContext";
 
 export default function RootLayout() {
   return (
     <GestureHandlerRootView>
       <AuthProvider>
         <TripProvider>
-          <Slot />
+          <StopProvider>
+            <Slot />
+          </StopProvider>
         </TripProvider>
       </AuthProvider>
     </GestureHandlerRootView>
