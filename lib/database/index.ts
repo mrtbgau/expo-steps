490520eae6407b34452d6346b5e5894eddc8e349
mrtbgau <mrt.bgau@gmail.com>
2025-10-12 13:21:12 +0200

    Export journalService and update type exports to include JournalEntry and JournalPhoto

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,5 +1,6 @@
 export { userService } from "./users";
 export { tripService } from "./trips";
 export { stopService } from "./stops";
+export { journalService } from "./journal";
 export { dbConnection } from "./connection";
-export type { User, Trip, Stop } from "./types";
+export type { User, Trip, Stop, JournalEntry, JournalPhoto } from "./types";
