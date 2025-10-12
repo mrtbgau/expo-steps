e4b01a71bfebfdec3aa3a26ae2ff27c22601e554
mrtbgau <mrt.bgau@gmail.com>
2025-10-12 15:14:09 +0200

    Export sharingService and update type exports for trip sharing functionality

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -3,5 +3,6 @@ export { tripService } from "./trips";
 export { stopService } from "./stops";
 export { journalService } from "./journal";
 export { checklistService } from "./checklist";
+export { sharingService } from "./sharing";
 export { dbConnection } from "./connection";
-export type { User, Trip, Stop, JournalEntry, JournalPhoto, ChecklistCategory, ChecklistItem, ChecklistReminder } from "./types";
+export type { User, Trip, Stop, JournalEntry, JournalPhoto, ChecklistCategory, ChecklistItem, ChecklistReminder, TripShare, TripCollaborator } from "./types";
