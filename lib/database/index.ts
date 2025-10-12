e33ddfca939c06368f2b5e67297057bdd2ebc5ed
mrtbgau <mrt.bgau@gmail.com>
2025-10-12 14:38:10 +0200

    Export Checklist types and service in database index for improved type management

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -2,5 +2,6 @@ export { userService } from "./users";
 export { tripService } from "./trips";
 export { stopService } from "./stops";
 export { journalService } from "./journal";
+export { checklistService } from "./checklist";
 export { dbConnection } from "./connection";
-export type { User, Trip, Stop, JournalEntry, JournalPhoto } from "./types";
+export type { User, Trip, Stop, JournalEntry, JournalPhoto, ChecklistCategory, ChecklistItem, ChecklistReminder } from "./types";
